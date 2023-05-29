f984faf43d19e218ac280065fb31805b1b519421
Abner Ismael Sul Felipe <abnersul80@gmail.com>
2023-05-28 19:29:14 -0600

    Complete themes

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,4 @@
+let themes = document.querySelector(".theme");
 let toggle = document.querySelector(".switch");
 let cont = 0;
 
@@ -5,10 +6,13 @@ toggle.addEventListener("click", () => {
   cont++;
   if (cont == 1) {
     toggle.className = "switch-2";
+    themes.className = "theme-two";
   } else if (cont == 2) {
     toggle.className = "switch-3";
+    themes.className = "theme-three";
   } else {
     toggle.className = "switch";
+    themes.className = "theme";
     cont = 0;
   }
 });
